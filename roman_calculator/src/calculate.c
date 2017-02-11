07767544b08ff9d40d8cb8494539597138184b7b
prahaladv <prahalad.venkat@gmail.com>
2017-02-10 23:32:42 -0500

    Created test suite for calculate and changed configure.ac
    - Compilation passes but the test case fails, owing to
      missing implementation.

---
+++ roman_calculator/src/calculate.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "calculate.h"
+
+int calculate_sum(char * pcNum1, char * pcNum2, char * pcSum)
+{
+    return 0;
+}
--- roman_calculator/tests/check_calculate.c
+++ roman_calculator/tests/check_calculate.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <check.h>
 #include "../src/calculate.h"
 
@@ -19,7 +20,31 @@ START_TEST(test_calculate_sum)
 }
 END_TEST
 
+Suite * calculate_suite(void)
+{
+    Suite * s;
+    TCase * tc_core;
+    s = suite_create("Calculate");
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_calculate_sum);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
 int main(void)
 {
-    return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = calculate_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
