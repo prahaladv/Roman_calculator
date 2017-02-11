9b5f0f4849a84ceb1d0de414233e23269a26e7a2
prahaladv <prahalad.venkat@gmail.com>
2017-02-10 22:34:27 -0500

    Added code in the header for test 1 compilation to pass
    - Compilation passes. But no implementation in the .c file yet.

--- roman_calculator/src/calculate.h
+++ roman_calculator/src/calculate.h
@@ -1,4 +1,15 @@
 #ifndef CALCULATE_H
 #define CALCULATE_H
 
+/* After making test 1, these are the macros that could be useful */
+#define SUCCESS 0
+#define FAILURE 1
+#define INVALID_NUM_1 2
+#define INVALID_NUM_2 3
+#define OUT_OF_BOUNDS 4
+
+#define MAX_LEN 40
+
+int calculate_sum(char * pcNum1, char * pcNum2, char * pcSum);
+
 #endif /* CALCULATE_H */
