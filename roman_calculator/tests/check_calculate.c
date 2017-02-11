f205708ff69ff86c8d4ec726499ea291b49e5647
prahaladv <prahalad.venkat@gmail.com>
2017-02-10 22:18:06 -0500

    Added test 1 to check sum of I and I
    - Compilation fails since there is no source code yet.

--- roman_calculator/tests/check_calculate.c
+++ roman_calculator/tests/check_calculate.c
@@ -1,3 +1,24 @@
+#include <check.h>
+#include "../src/calculate.h"
+
+/* Test 1 */
+START_TEST(test_calculate_sum)
+{
+    int iRetVal = FAILURE;
+    char * pcInput1 = "I";
+    char * pcInput2 = "I";
+
+    /* Assuming the max length of a Roman string will be MAX_LEN = 40 */
+    char acSum[MAX_LEN] = {0};
+
+    /* Should take in two input strings and return output in acSum - a valid buffer */
+    iRetVal = calculate_sum(pcInput1, pcInput2, acSum);
+
+    ck_assert_int_eq(iRetVal, SUCCESS);
+    ck_assert_str_eq(acSum, "II");
+}
+END_TEST
+
 int main(void)
 {
     return 0;
