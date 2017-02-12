baa260e01ad89c12d48f827be10afd98961b444f
prahaladv <prahalad.venkat@gmail.com>
2017-02-12 02:31:53 -0500

    Added test 9 to check find_value_of_string() ret val
    - Test to check the return value of the function when
      it is passed a simple string without amy subtractibles
      such as "IX" (9, not 11)  or "CD" (400, not 600).

--- roman_calculator/tests/check_calculate.c
+++ roman_calculator/tests/check_calculate.c
@@ -124,6 +124,21 @@ START_TEST(test_value_of_string_fails_if_passed_null_int_pointer)
 }
 END_TEST
 
+/* Test 9 */
+START_TEST(test_value_of_string_on_passing_a_valid_string_without_subtractibles)
+{
+    int iRetVal = SUCCESS;
+    int iValue = 0;
+
+    /* Should take in one input string and pointer to an int variable
+     * and return appropriate output */
+    iRetVal = find_value_of_string("MDCLXVI", &iValue);
+
+    ck_assert_int_eq(iRetVal, SUCCESS);
+    ck_assert_int_eq(iValue, 1666);
+}
+END_TEST
+
 Suite * calculate_suite(void)
 {
     Suite * s;
@@ -138,9 +153,11 @@ Suite * calculate_suite(void)
     //tcase_add_test(tc_core, test_check_if_roman_string_fails_if_passed_null);
     //tcase_add_test(tc_core, test_check_if_roman_string_fails_if_passed_random_string);
 
-    tcase_add_test(tc_core, test_value_of_string_fails_if_passed_null);
-    tcase_add_test(tc_core, test_value_of_string_fails_if_passed_empty_string);
-    tcase_add_test(tc_core, test_value_of_string_fails_if_passed_null_int_pointer);
+    //tcase_add_test(tc_core, test_value_of_string_fails_if_passed_null);
+    //tcase_add_test(tc_core, test_value_of_string_fails_if_passed_empty_string);
+    //tcase_add_test(tc_core, test_value_of_string_fails_if_passed_null_int_pointer);
+
+    tcase_add_test(tc_core, test_value_of_string_on_passing_a_valid_string_without_subtractibles);
 
     suite_add_tcase(s, tc_core);
 
