359f152dab3db2bbd24de2306ca1c67bdd68977e
prahaladv <prahalad.venkat@gmail.com>
2017-02-11 23:02:57 -0500

    Implemented calculate_diff() and test 2 passes

--- roman_calculator/src/calculate.c
+++ roman_calculator/src/calculate.c
@@ -13,6 +13,8 @@ int find_value_of_string(char * acNum, int * piVal)
 {
     if(piVal != NULL && !strcmp(acNum, "I"))
         *piVal = 1;
+    else if(piVal != NULL && !strcmp(acNum, "III"))
+        *piVal = 3;
 
     return SUCCESS;
 }
@@ -75,7 +77,56 @@ int calculate_sum(char * pcNum1, char * pcNum2, char * pcSum)
     return iRetVal;
 }
 
-int calculate_diff(char * pNum1, char * pcNum2, char * pcDiff)
+int calculate_diff(char * pcNum1, char * pcNum2, char * pcDiff)
 {
-    return 0;
+    int iLen1 = strlen(pcNum1), iLen2 = strlen(pcNum2);
+    char acNum1[iLen1 + 1];
+    char acNum2[iLen2 + 1];
+    strcpy(acNum1, pcNum1);
+    strcpy(acNum2, pcNum2);
+    int iNum1 = 0, iNum2 = 0, iDiff = 0, iRetVal = SUCCESS;
+
+    /* To check if the input string 1 contains only roman characters */
+    iRetVal = check_if_roman_string(acNum1);
+    if(iRetVal != SUCCESS)
+    {
+        iRetVal = INVALID_NUM_1;
+        return iRetVal;
+    }
+    /* To check if the input string 2 contains only roman characters */
+    iRetVal = check_if_roman_string(acNum2);
+    if(iRetVal != SUCCESS)
+    {
+        iRetVal = INVALID_NUM_2;
+        return iRetVal;
+    }
+
+    /* To fetch Roman string 1's value */
+    iRetVal = find_value_of_string(acNum1, &iNum1);
+    if(iRetVal != SUCCESS)
+    {
+        iRetVal = INVALID_NUM_1;
+        return iRetVal;
+    }
+    /* To fetch Roman string 2's value */
+    iRetVal = find_value_of_string(acNum2, &iNum2);
+    if(iRetVal != SUCCESS)
+    {
+        iRetVal = INVALID_NUM_2;
+        return iRetVal;
+    }
+
+    printf("\nNumber 1 : %d, Number 2: %d\n", iNum1, iNum2);
+
+    iDiff = iNum1 - iNum2;
+
+    printf("\nDifference: %d\n", iDiff);
+
+    iRetVal = convert_to_roman_string(iDiff, pcDiff);
+    if(iRetVal != SUCCESS)
+    {
+        return iRetVal;
+    }
+
+    return iRetVal;
 }
