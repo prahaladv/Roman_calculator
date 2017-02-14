9a7a0aabc7a21a4d3184b703a830c23606564ee5
prahaladv <prahalad.venkat@gmail.com>
2017-02-14 02:21:44 -0500

    Added application functionality in src/main.c
    - Added code in the main function that takes in two inputs and
      finds the sum (Num 1 + Num 2) and difference (Num 1 - Num 2).
    - It displays the result or an appropriate msg in case of errors.

--- roman_calculator/src/main.c
+++ roman_calculator/src/main.c
@@ -1,11 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "calculate.h"
 
-/* only main should be in this file, to make all other functions in
-   the prograble testable by Check.  in order to test main(), use a
-   whole program testing framework like Autotest.
-*/
-
-int main (void)
+void print_error_and_exit(int err)
 {
-  return 0;
+    if(err == FAILURE)
+    {
+        printf("The calculation failed. Please retry.\n");
+    }
+    else if(err == OUT_OF_BOUNDS)
+    {
+        printf("The calculation failed due to out of bounds output. Please retry.\n");
+    }
+    else if(err == INVALID_INPUT)
+    {
+        printf("The calculation failed due to invalid inputs. Please retry.\n");
+    }
+    else if(err == INVALID_NUM_1)
+    {
+        printf("The calculation failed due to invalid input 1. Please retry.\n");
+    }
+    else if(err == INVALID_NUM_2)
+    {
+        printf("The calculation failed due to invalid input 2. Please retry.\n");
+    }
+    _exit(EXIT_FAILURE);
+}
+
+int main(int argc, char * argv[])
+{
+    int iRetVal = SUCCESS;
+
+    if(argc != 3)
+    {
+        printf("Incorrect number of arguments.\nFormat: ./main <Roman_string_1> <Romand_string_2>\nPlease retry.\n");
+        return 1;
+    }
+
+    char acSum[MAX_LEN] = {0};
+    char acDiff[MAX_LEN] = {0};
+
+    iRetVal = calculate_sum(argv[1], argv[2], (char *) &acSum);
+
+    if(iRetVal != SUCCESS)
+    {
+        print_error_and_exit(iRetVal);
+    }
+
+    if(iRetVal == SUCCESS)
+    {
+        strcmp(acSum, "") ? printf("The sum is: %s\n\n", acSum) :
+            printf("The sum is: -\n");
+    }
+
+    iRetVal = calculate_diff(argv[1], argv[2], (char *) &acDiff);
+    if(iRetVal != SUCCESS && iRetVal != NEGATIVE_DIFFERENCE)
+    {
+        print_error_and_exit(iRetVal);
+    }
+
+    if(iRetVal == SUCCESS)
+    {
+        strcmp(acDiff, "") ? printf("The difference is: %s\n\n", acDiff) :
+            printf("The difference is: -\n");
+    }
+    else if(iRetVal == NEGATIVE_DIFFERENCE)
+    {
+        printf("The difference is: Negative %s\n\n", acDiff);
+    }
+
+    return SUCCESS;
 }
